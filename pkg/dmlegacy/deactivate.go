b045122a4a5f63576a58e17fc3730c219836befc
Erik Seliger <erikseliger@me.com>
2022-08-18 20:13:50 +0200

    Try to fix dmsetup device busy errors (#8)
    * Revert "Add more debugging context to dmsetup remove (#7)"
    
    This reverts commit 543bdae37fe235a1d78d093399bb310daa13a1d1.
    
    * Try to fix dmsetup device busy errors
    
    With a test script, I was able to reproduce this race condition and it seems to go away when we separately remove the devices in the counter-order they depend on each other.

--- pkg/dmlegacy/deactivate.go
+++ pkg/dmlegacy/deactivate.go
@@ -1,26 +1,24 @@
 package dmlegacy
 
 import (
-	"path"
+	"context"
+	"errors"
+	"fmt"
 	"strings"
 
-	log "github.com/sirupsen/logrus"
 	api "github.com/weaveworks/ignite/pkg/apis/ignite"
 	"github.com/weaveworks/ignite/pkg/util"
 )
 
 // dmsetupNotFound is the error message when dmsetup can't find a device.
 const dmsetupNotFound = "No such device or address"
-const dmsetupResourceBusy = "Device or resource busy"
 
 // DeactivateSnapshot deactivates the snapshot by removing it with dmsetup. The
 // loop device will automatically be cleaned, since it has been detached before.
 func DeactivateSnapshot(vm *api.VM) error {
-	dmArgs := []string{
-		"remove",
-		"--retry",      // udev might hold a lock briefly, so we have to retry to make sure this can work.
-		"--verifyudev", // if udevd is not running, dmsetup will manage the device node in /dev/mapper
-		vm.PrefixedID(),
+	var errs []error
+	if err := removeDeviceMapping(context.Background(), vm.PrefixedID()); err != nil {
+		errs = append(errs, err)
 	}
 
 	// If the base device is visible in "dmsetup", we should remove it
@@ -28,32 +26,32 @@ func DeactivateSnapshot(vm *api.VM) error {
 	// TODO: Improve this detection
 	baseDev := vm.NewPrefixer().Prefix(vm.GetUID(), "base")
 	if _, err := util.ExecuteCommand("dmsetup", "info", baseDev); err == nil {
-		dmArgs = append(dmArgs, baseDev)
+		if err := removeDeviceMapping(context.Background(), baseDev); err != nil {
+			errs = append(errs, err)
+		}
 	}
 
+	if len(errs) > 0 {
+		errStr := "failed to deactivate snapshot:"
+		for _, err := range errs {
+			errStr += fmt.Sprintf("\n - %s", err.Error())
+		}
+		return errors.New(errStr)
+	}
+	return nil
+}
+
+func removeDeviceMapping(ctx context.Context, name string) error {
+	dmArgs := []string{
+		"remove",
+		"--retry",      // udev might hold a lock briefly, so we have to retry to make sure this can work.
+		"--verifyudev", // if udevd is not running, dmsetup will manage the device node in /dev/mapper
+		name,
+	}
 	if _, err := util.ExecuteCommand("dmsetup", dmArgs...); err != nil {
 		// If the device is not found, it's been deleted already, return nil.
 		if strings.Contains(err.Error(), dmsetupNotFound) {
 			return nil
-		}
-		if strings.Contains(err.Error(), dmsetupResourceBusy) {
-			log.Warnf("Resource busy error encountered")
-			{
-				out, err2 := util.ExecuteCommand("dmsetup", "info", "-c", vm.PrefixedID(), baseDev)
-				if err2 != nil {
-					log.Warnf("Failed to get more info on device or resource busy error: dmsetup info: %s", err2)
-				}
-				log.Warnf("dmsetup info returns: %s", out)
-			}
-
-			{
-				out, err2 := util.ExecuteCommand("lsof", path.Join("/dev/mapper", vm.PrefixedID()))
-				if err2 != nil {
-					log.Warnf("Failed to get more info on device or resource busy error: lsof: %s", err2)
-				}
-				log.Warnf("potential users of the file: %s", out)
-
-			}
 		}
 		return err
 	}
